dff80e292d362daace0cdd1838bed07923f45cd1
kimu_shu <alfvegardrisc@gmail.com>
2017-06-04 16:06:25 +0900

    Add util.promisify()

--- src/node/dux_util.c
+++ src/node/dux_util.c
@@ -8,6 +8,9 @@
 #if !defined(DUX_OPT_NO_NODEJS_MODULES) && !defined(DUX_OPT_NO_UTIL)
 #include "../dux_internal.h"
 
+DUK_LOCAL const char DUX_IPK_PROMISIFY_FUNC[] = DUX_IPK("upFunc");
+DUK_LOCAL const char DUX_SYM_PROMISIFY_CUSTOM[] = "\x80util.promisify.custom";
+
 /*
  * Entry of util.format()
  */
@@ -95,6 +98,115 @@ DUK_LOCAL duk_ret_t util_inspect(duk_context *ctx)
 	return DUK_RET_ERROR;
 }
 
+#if !defined(DUX_OPT_NO_PROMISE)
+/*
+ * Callback for promisified function
+ */
+DUK_LOCAL duk_ret_t util_promisify_callback(duk_context *ctx)
+{
+	/* [ resolve reject err result ] */
+	if (duk_is_null_or_undefined(ctx, 2)) {
+		/* Fulfilled */
+		duk_swap(ctx, 0, 2);
+		/* [ null|undefined reject resolve result ] */
+		duk_call(ctx, 1);
+	} else {
+		/* Rejected */
+		duk_pop(ctx);
+		/* [ resolve reject err ] */
+		duk_call(ctx, 1);
+	}
+	return 0;
+}
+
+/*
+ * Executor of promisified function
+ */
+DUK_LOCAL duk_ret_t util_promisify_executor(duk_context *ctx)
+{
+	duk_context *sub;
+
+	/* ctx: [ thread resolve reject ] */
+	sub = duk_require_context(ctx, 0);
+	duk_push_c_function(sub, util_promisify_callback, 4);
+	duk_xcopy_top(sub, ctx, 2);
+	/* sub: [ func this ... callback resolve reject ] */
+	dux_bind_arguments(sub, 2);
+	/* sub: [ func this ... bound_callback ] */
+	if (duk_pcall_method(sub, duk_get_top(sub) - 2) != DUK_EXEC_SUCCESS) {
+		/* sub: [ err ] */
+		duk_xmove_top(ctx, sub, 1);
+		/* ctx: [ thread resolve reject err ] */
+		/* sub: [  ] */
+		duk_call(ctx, 1);
+	}
+	return 0;
+}
+
+/*
+ * Entry of promisified function
+ */
+DUK_LOCAL duk_ret_t util_promisify_invoker(duk_context *ctx)
+{
+	duk_context *sub;
+
+	/* [ ... ] */
+	duk_push_thread(ctx);
+	duk_insert(ctx, 0);
+	/* [ thread ... ] */
+	duk_push_current_function(ctx);
+	/* [ thread ... invoker ] */
+	duk_get_prop_string(ctx, -1, DUX_IPK_PROMISIFY_FUNC);
+	duk_push_this(ctx);
+	/* [ thread ... invoker func this ] */
+	sub = duk_require_context(ctx, 0);
+	duk_xmove_top(sub, ctx, 2);
+	duk_pop(ctx);
+	duk_xmove_top(sub, ctx, duk_get_top(ctx) - 1);
+	/* ctx: [ thread ] */
+	/* sub: [ func this ... ] */
+
+	duk_get_global_string(ctx, "Promise");
+	/* ctx: [ thread Promise ] */
+	duk_push_c_function(ctx, util_promisify_executor, 3);
+	duk_dup(ctx, 0);
+	/* ctx: [ thread Promise executor thread ] */
+	dux_bind_arguments(ctx, 1);
+	/* ctx: [ thread Promise bound_executor ] */
+	duk_new(ctx, 1);
+	/* ctx: [ thread promise ] */
+	return 1;
+}
+
+/*
+ * Entry of util.promisify()
+ */
+DUK_LOCAL duk_ret_t util_promisify(duk_context *ctx)
+{
+	/* [ func ] */
+	duk_require_function(ctx, 0);
+	if (duk_get_prop_string(ctx, 0, DUX_SYM_PROMISIFY_CUSTOM)) {
+		/* func already has promisified function */
+		/* [ func promisified_func ] */
+		return 1;
+	}
+	duk_pop(ctx);
+
+	duk_get_prop_string(ctx, 0, "length");
+	/* [ func length ] */
+	duk_push_c_function(ctx, util_promisify_invoker, DUK_VARARGS);
+	duk_swap(ctx, 1, 2);
+	/* [ length invoker func ] */
+	duk_put_prop_string(ctx, 1, DUX_IPK_PROMISIFY_FUNC);
+	/* [ length invoker ] */
+	duk_swap(ctx, 0, 1);
+	/* [ invoker length ] */
+	duk_put_prop_string(ctx, 0, "length");
+	/* [ invoker ] */
+	return 1;
+}
+#endif	/* !DUX_OPT_NO_PROMISE */
+
 /*
  * List of methods for Util object
  */
@@ -113,6 +225,17 @@ DUK_INTERNAL duk_errcode_t dux_util_init(duk_context *ctx)
 	duk_push_object(ctx);
 	/* [ ... obj ] */
 	duk_put_function_list(ctx, -1, util_funcs);
+#if !defined(DUX_OPT_NO_PROMISE)
+	duk_push_c_function(ctx, util_promisify, 1);
+	/* [ ... util func ] */
+	duk_put_prop_string(ctx, -2, "custom");
+	duk_push_string(ctx, DUX_SYM_PROMISIFY_CUSTOM);
+	/* [ ... util func "custom" symbol ] */
+	duk_def_prop(ctx, -3, DUK_DEFPROP_HAVE_VALUE |
+		DUK_DEFPROP_CLEAR_WRITABLE | DUK_DEFPROP_SET_ENUMERABLE);
+	/* [ ... util func ] */
+	duk_put_prop_string(ctx, -2, "promisify");
+#endif	/* !DUX_OPT_NO_PROMISE */
 	/* [ ... util ] */
 	duk_put_global_string(ctx, "util");
 	/* [ ... ] */
