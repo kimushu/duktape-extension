3dc70d07f03ffc1843fd556cd3c71de5ce257af9
kimu_shu <alfvegardrisc@gmail.com>
2017-07-21 02:07:32 +0900

    Add NodeJS 6.x buffer prototypes

--- test/typings/globals/duktape/index.d.ts
+++ test/typings/globals/duktape/index.d.ts
@@ -161,7 +161,358 @@ declare namespace Reflect {
  * Node.js like objects
  */
 
-interface Buffer extends Uint8Array {}
+interface Buffer extends Uint8Array {
+    /**
+     * Reads a unsigned 8-bit integer from buf at the specified offset
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readUInt8(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a signed 8-bit integer from buf at the specified offset
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readInt8(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a unsigned 16-bit integer from buf at the specified offset with big-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readUInt16BE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a unsigned 16-bit integer from buf at the specified offset with little-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readUInt16LE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a signed 16-bit integer from buf at the specified offset with big-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readInt16BE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a signed 16-bit integer from buf at the specified offset with little-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readInt16LE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a unsigned 32-bit integer from buf at the specified offset with big-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readUInt32BE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a unsigned 32-bit integer from buf at the specified offset with little-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readUInt32LE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a signed 32-bit integer from buf at the specified offset with big-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readInt32BE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a signed 32-bit integer from buf at the specified offset with little-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readInt32LE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a 32-bit float from buf at the specified offset with big-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readFloatBE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a 32-bit float from buf at the specified offset with little-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readFloatLE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a 64-bit double from buf at the specified offset with big-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readDoubleBE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads a 64-bit double from buf at the specified offset with little-endian format
+     * @param offset Where to start reading
+     * @param noAssert Skip offset validation
+     */
+    readDoubleLE(offset: number, noAssert?: boolean): number;
+
+    /**
+     * Reads byteLength number of bytes from buf at the specified offset with big-endian format
+     * and interprets the result as an unsigned integer
+     * @param offset Where to start reading
+     * @param byteLength How many bytes to read
+     * @param noAssert Skip offset validation
+     */
+    readUIntBE(offset: number, byteLength: number, noAssert?: boolean): number;
+
+    /**
+     * Reads byteLength number of bytes from buf at the specified offset with little-endian format
+     * and interprets the result as an unsigned integer
+     * @param offset Where to start reading
+     * @param byteLength How many bytes to read
+     * @param noAssert Skip offset validation
+     */
+    readUIntLE(offset: number, byteLength: number, noAssert?: boolean): number;
+
+    /**
+     * Reads byteLength number of bytes from buf at the specified offset with big-endian format
+     * and interprets the result as a two's complement signed value (Up to 48 bits accuracy)
+     * @param offset Where to start reading
+     * @param byteLength How many bytes to read
+     * @param noAssert Skip offset validation
+     */
+    readIntBE(offset: number, byteLength: number, noAssert?: boolean): number;
+
+    /**
+     * Reads byteLength number of bytes from buf at the specified offset with little-endian format
+     * and interprets the result as a two's complement signed value (Up to 48 bits accuracy)
+     * @param offset Where to start reading
+     * @param byteLength How many bytes to read
+     * @param noAssert Skip offset validation
+     */
+    readIntLE(offset: number, byteLength: number, noAssert?: boolean): number;
+
+    /**
+     * Writes unsigned 8-bit value to buf at the specified offset
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeUInt8(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes signed 8-bit value to buf at the specified offset
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeInt8(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes unsigned 16-bit value to buf at the specified offset with big-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeUInt16BE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes unsigned 16-bit value to buf at the specified offset with little-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeUInt16LE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes signed 16-bit value to buf at the specified offset with big-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeInt16BE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes signed 16-bit value to buf at the specified offset with little-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeInt16LE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes unsigned 32-bit value to buf at the specified offset with big-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeUInt32BE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes unsigned 32-bit value to buf at the specified offset with little-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeUInt32LE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes signed 32-bit value to buf at the specified offset with big-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeInt32BE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes signed 32-bit value to buf at the specified offset with little-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeInt32LE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes 32-bit float value to buf at the specified offset with big-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeFloatBE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes 32-bit float value to buf at the specified offset with little-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeFloatLE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes 64-bit double value to buf at the specified offset with big-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeDoubleBE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes 64-bit double value to buf at the specified offset with little-endian format
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeDoubleLE(value: number, offset: number, noAssert?: boolean): number;
+
+    /**
+     * Writes byteLength bytes of unsigned value to buf at the specified offset with big-endian format (Up to 48 bits accuracy)
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param byteLength How many bytes to write
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeUIntBE(value: number, offset: number, byteLength: number, noAssert?: boolean): number;
+
+    /**
+     * Writes byteLength bytes of unsigned value to buf at the specified offset with little-endian format (Up to 48 bits accuracy)
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param byteLength How many bytes to write
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeUIntLE(value: number, offset: number, byteLength: number, noAssert?: boolean): number;
+
+    /**
+     * Writes byteLength bytes of signed value to buf at the specified offset with big-endian format (Up to 48 bits accuracy)
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param byteLength How many bytes to write
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeIntBE(value: number, offset: number, byteLength: number, noAssert?: boolean): number;
+
+    /**
+     * Writes byteLength bytes of signed value to buf at the specified offset with little-endian format (Up to 48 bits accuracy)
+     * @param value Number to be written to buf
+     * @param offset Where to start writing
+     * @param byteLength How many bytes to write
+     * @param noAssert Skip value and offset validation
+     * @return offset plus number of bytes written
+     */
+    writeIntLE(value: number, offset: number, byteLength: number, noAssert?: boolean): number;
+
+    /**
+     * Returns a JSON representation of buf
+     */
+    toJSON(): Object;
+
+    /**
+     * Returns true if both buf and otherBuffer have exactly the same bytes, false otherwise
+     * @param otherBuffer A Buffer to compare to
+     */
+    equals(otherBuffer: Buffer): boolean;
+
+    /**
+     * Compares buf with target and returns a number indicating whether buf comes becore, after, or it the same
+     * as target in sort order. Comparison is based on the acutual sequence of bytes in each Buffer
+     * @param target A Buffer to compare to
+     * @param targetStart The offset within target at which to begin comparison
+     * @param targetEnd The offset with target at which to end comparison (not inclusive)
+     * @param sourceStart The offset within buf at which to begin comparison
+     * @param sourceEnd The offset within buf at which to end comparison (not inclusive)
+     * @return 0: target is the same as buf, 1: target should come before buf when sorted, -1: target should come after buf when sorted
+     */
+    compare(target: Buffer, targetStart?: number, targetEnd?: number, sourceStart?: number, sourceEnd?: number): number;
+
+    /**
+     * Copies data from a region of buf to a region in target even if the target memory region overlaps with buf
+     * @param target A Buffer or Uint8Array to copy into
+     * @param targetStart The offset within target at which to begin copying to
+     * @param sourceStart The offset within buf at which to begin copying from
+     * @param sourceEnd The offset within buf at which to stop copying (not inclusive)
+     * @return The number of bytes copied
+     */
+    copy(target: Buffer, targetStart?: number, sourceStart?: number, sourceEnd?: number): number;
+
+    /**
+     * Returns a new Buffer that references the same memory as the original,
+     * but offset and cropped by the start and end indices
+     * @param start Where the new Buffer will start
+     * @param end Where the new Buffer will end (not inclusive)
+     */
+    slice(start?: number, end?: number): Buffer;
+
+    /**
+     * Writes string to buf at the offset according to the specified character encoding
+     * @param string String to be written to buf
+     * @param offset Where to start writing string
+     * @param length How many bytes to write
+     * @param encoding The character encoding of string (only "utf8" is accepted in Duktape)
+     * @return Number of bytes written
+     */
+    write(string: string, offset?: number, length?: number, encoding?: string): number;
+}
 
 interface BufferConstructor {
     readonly prototype: Buffer;
